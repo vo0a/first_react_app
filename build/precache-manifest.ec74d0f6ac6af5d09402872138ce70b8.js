39dea0242ed021fc7b59a42e90591c8750b2f5f7
vo0a <vo0a195@gmail.com>
2018-12-01 13:08:28 +0900

    Add build directory

---
+++ build/precache-manifest.ec74d0f6ac6af5d09402872138ce70b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0cf67cc12cb5eabd3142",
+    "url": "/static/js/main.0cf67cc1.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "0cf67cc12cb5eabd3142",
+    "url": "/static/css/main.451fe0e6.chunk.css"
+  },
+  {
+    "revision": "8010da963271b12d86b054785a1639c3",
+    "url": "/index.html"
+  }
+];
